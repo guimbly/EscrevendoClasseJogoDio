dfbf5022287013bac7552be4848fd14dc8cfaf5b
guimbly <pietropaiva.pp@hotmail.com>
2023-11-06 16:39:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+class hero{
+    constructor(heroName, heroAge, heroClass){
+        this.heroName = heroName;
+        this.heroAge = heroAge;
+        this.heroClass = heroClass.toString().toLowerCase();
+    }
+    attack(){
+
+        if(this.heroClass == 'mago'){
+            console.log(`o ${this.heroClass} atacou usando magia!`)
+        }
+        else if(this.heroClass == 'ninja'){
+            console.log(`o ${this.heroClass} atacou usando shurikens!`)
+        }
+        else if(this.heroClass == 'monge'){
+            console.log(`o ${this.heroClass} atacou usando os punhos!`)
+        }
+        else if(this.heroClass == 'guerreiro'){
+            console.log(`o ${this.heroClass} atacou usando a espada!`)
+        }
+        else{
+            console.log(`a classe secreta ${this.heroClass} atacou usando o ataque secreto!`)
+        }
+    }
+}
+let a = new hero("a", 25, "GueRReiro")
+a.attack()
